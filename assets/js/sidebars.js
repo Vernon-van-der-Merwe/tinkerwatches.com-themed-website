627fced0b81a83c55ec735c89aae06c55072cbc4
Vernon van der Merwe <Vernon.vd.m14@gmail.com>
2020-05-02 01:53:56 +0200

    Bianca

---
+++ assets/js/sidebars.js
@@ -0,0 +1,18 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+	document.getElementById('mySidenav').style.width = '350px'
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+	document.getElementById('mySidenav').style.width = '0'
+}
+/* Set the width of the side navigation to 250px */
+function openCart() {
+	document.getElementById('mySideCart').style.width = '350px'
+}
+
+/* Set the width of the side navigation to 0 */
+function closeCart() {
+	document.getElementById('mySideCart').style.width = '0'
+}
